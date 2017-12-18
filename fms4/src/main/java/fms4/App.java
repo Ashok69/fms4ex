e1059a6a02be25a3219355808477830c58205c3c
Ashok <ashokrevelli70@gmail.com>
2017-12-19 00:22:31 +0530

    added fms4 file

---
+++ fms4/src/main/java/fms4/App.java
@@ -0,0 +1,11 @@
+package fms4;
+
+public class App {
+
+	public static void main( String[] args)
+{
+	System.out.println("Hello world");
+
+	
+}
+}
---
+++ fms4/src/test/java/fms4/AppTest.java
@@ -0,0 +1,14 @@
+package fms4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+
+	}
+
+}
